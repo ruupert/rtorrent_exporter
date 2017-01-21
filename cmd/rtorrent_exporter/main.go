b6dd45f0a444a29f9584f8d925d51e479a0e0c0e
Matt Layher <mdlayher@gmail.com>
2017-01-20 19:11:57 -0500

    rtorrent_exporter: add flags for HTTP Basic authentication

--- cmd/rtorrent_exporter/main.go
+++ cmd/rtorrent_exporter/main.go
@@ -15,7 +15,9 @@ var (
 	telemetryAddr = flag.String("telemetry.addr", ":9135", "host:port for rTorrent exporter")
 	metricsPath   = flag.String("telemetry.path", "/metrics", "URL path for surfacing collected metrics")
 
-	rtorrentAddr = flag.String("rtorrent.addr", "", "address of rTorrent XML-RPC server")
+	rtorrentAddr     = flag.String("rtorrent.addr", "", "address of rTorrent XML-RPC server")
+	rtorrentUsername = flag.String("rtorrent.username", "", "[optional] username used for HTTP Basic authentication with rTorrent XML-RPC server")
+	rtorrentPassword = flag.String("rtorrent.password", "", "[optional] password used for HTTP Basic authentication with rTorrent XML-RPC server")
 )
 
 func main() {
@@ -25,7 +27,16 @@ func main() {
 		log.Fatal("address of rTorrent XML-RPC server must be specified with '-rtorrent.addr' flag")
 	}
 
-	c, err := rtorrent.New(*rtorrentAddr, nil)
+	// Optionally enable HTTP Basic authentication
+	var rt http.RoundTripper
+	if u, p := *rtorrentUsername, *rtorrentPassword; u != "" && p != "" {
+		rt = &authRoundTripper{
+			Username: u,
+			Password: p,
+		}
+	}
+
+	c, err := rtorrent.New(*rtorrentAddr, rt)
 	if err != nil {
 		log.Fatalf("cannot create rTorrent client: %v", err)
 	}
@@ -37,9 +48,24 @@ func main() {
 		http.Redirect(w, r, *metricsPath, http.StatusMovedPermanently)
 	})
 
-	log.Printf("starting rTorrent exporter on %q for server %q", *telemetryAddr, *rtorrentAddr)
+	log.Printf("starting rTorrent exporter on %q for server %q (authentication: %v)",
+		*telemetryAddr, *rtorrentAddr, rt != nil)
 
 	if err := http.ListenAndServe(*telemetryAddr, nil); err != nil {
 		log.Fatalf("cannot start rTorrent exporter: %s", err)
 	}
+}
+
+var _ http.RoundTripper = &authRoundTripper{}
+
+// An authRoundTripper is a http.RoundTripper which adds HTTP Basic authentication
+// to each HTTP request.
+type authRoundTripper struct {
+	Username string
+	Password string
+}
+
+func (rt *authRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+	r.SetBasicAuth(rt.Username, rt.Password)
+	return http.DefaultTransport.RoundTrip(r)
 }
