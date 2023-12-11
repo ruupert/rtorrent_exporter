7861fde493145efcf6bf548ac2cb011572b10894
Raul Becker <raul.becker@iki.fi>
2023-12-12 01:36:23 +0200

    using exporter toolkit to enable tls

--- cmd/rtorrent_exporter/main.go
+++ cmd/rtorrent_exporter/main.go
@@ -2,30 +2,52 @@
 package main
 
 import (
-	"flag"
-	"log"
 	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"os/user"
+	"runtime"
 
+	"github.com/go-kit/log/level"
+
+	"github.com/alecthomas/kingpin/v2"
 	"github.com/mdlayher/rtorrent"
-	"github.com/mdlayher/rtorrent_exporter"
+	rtorrentexporter "github.com/mdlayher/rtorrent_exporter"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-)
-
-var (
-	telemetryAddr = flag.String("telemetry.addr", ":9135", "host:port for rTorrent exporter")
-	metricsPath   = flag.String("telemetry.path", "/metrics", "URL path for surfacing collected metrics")
-
-	rtorrentAddr     = flag.String("rtorrent.addr", "", "address of rTorrent XML-RPC server")
-	rtorrentUsername = flag.String("rtorrent.username", "", "[optional] username used for HTTP Basic authentication with rTorrent XML-RPC server")
-	rtorrentPassword = flag.String("rtorrent.password", "", "[optional] password used for HTTP Basic authentication with rTorrent XML-RPC server")
+	"github.com/prometheus/common/promlog"
+	"github.com/prometheus/common/promlog/flag"
+	"github.com/prometheus/common/version"
+	"github.com/prometheus/exporter-toolkit/web"
+	"github.com/prometheus/exporter-toolkit/web/kingpinflag"
 )
 
 func main() {
-	flag.Parse()
+	var (
+		toolkitFlags = kingpinflag.AddFlags(kingpin.CommandLine, ":9135")
+		metricsPath  = kingpin.Flag(
+			"web.telemetry-path",
+			"Path under which to expose metrics.",
+		).Default("/metrics").String()
+
+		rtorrentAddr     = kingpin.Flag("rtorrent.addr", "address of rTorrent XML-RPC server").Default("").String()
+		rtorrentUsername = kingpin.Flag("rtorrent.username", "[optional] username used for HTTP Basic authentication with rTorrent XML-RPC server").Default("").String()
+		rtorrentPassword = kingpin.Flag("rtorrent.password", "[optional] password used for HTTP Basic authentication with rTorrent XML-RPC server").Default("").String()
+		maxProcs         = kingpin.Flag(
+			"runtime.gomaxprocs", "The target number of CPUs Go will run on (GOMAXPROCS)",
+		).Envar("GOMAXPROCS").Default("1").Int()
+	)
+
+	promlogConfig := &promlog.Config{}
+	flag.AddFlags(kingpin.CommandLine, promlogConfig)
+	kingpin.Version(version.Print("rtorrent_exporter"))
+	kingpin.CommandLine.UsageWriter(os.Stdout)
+	kingpin.HelpFlag.Short('h')
+	kingpin.Parse()
+	logger := promlog.New(promlogConfig)
 
 	if *rtorrentAddr == "" {
-		log.Fatal("address of rTorrent XML-RPC server must be specified with '-rtorrent.addr' flag")
+		level.Error(logger).Log("address of rTorrent XML-RPC server must be specified with '-rtorrent.addr' flag")
 	}
 
 	// Optionally enable HTTP Basic authentication
@@ -39,21 +61,31 @@ func main() {
 
 	c, err := rtorrent.New(*rtorrentAddr, rt)
 	if err != nil {
-		log.Fatalf("cannot create rTorrent client: %v", err)
+		level.Error(logger).Log("cannot create rTorrent client: %v", err)
 	}
 
 	prometheus.MustRegister(rtorrentexporter.New(c))
+
+	level.Info(logger).Log("starting rTorrent exporter on %q for server %q (authentication: %v)",
+		*toolkitFlags, *rtorrentAddr, rt != nil)
+
+	level.Info(logger).Log("msg", "Starting rtorrent_exporter", "version", version.Info())
+	level.Info(logger).Log("msg", "Build context", "build_context", version.BuildContext())
+	if user, err := user.Current(); err == nil && user.Uid == "0" {
+		level.Warn(logger).Log("msg", "rTorrent Exporter is running as root user. This exporter is designed to run as unprivileged user, root is not required.")
+	}
+	runtime.GOMAXPROCS(*maxProcs)
+	level.Debug(logger).Log("msg", "Go MAXPROCS", "procs", runtime.GOMAXPROCS(0))
 
 	http.Handle(*metricsPath, promhttp.Handler())
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, *metricsPath, http.StatusMovedPermanently)
 	})
 
-	log.Printf("starting rTorrent exporter on %q for server %q (authentication: %v)",
-		*telemetryAddr, *rtorrentAddr, rt != nil)
-
-	if err := http.ListenAndServe(*telemetryAddr, nil); err != nil {
-		log.Fatalf("cannot start rTorrent exporter: %s", err)
+	server := &http.Server{}
+	if err := web.ListenAndServe(server, toolkitFlags, logger); err != nil {
+		level.Error(logger).Log("err", err)
+		os.Exit(1)
 	}
 }
 
--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module github.com/mdlayher/rtorrent_exporter
 go 1.13
 
 require (
+	github.com/alecthomas/kingpin/v2 v2.3.2
+	github.com/go-kit/log v0.2.1
 	github.com/mdlayher/rtorrent v0.0.0-20191011143022-58e7d496034e
-	github.com/prometheus/client_golang v1.1.0
-	github.com/prometheus/client_model v0.0.0-20190812154241-14fe0d1b01d4 // indirect
-	github.com/prometheus/common v0.7.0 // indirect
-	github.com/prometheus/procfs v0.0.5 // indirect
-	golang.org/x/sys v0.0.0-20191010194322-b09406accb47 // indirect
+	github.com/prometheus/client_golang v1.17.0
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.45.0
+	github.com/prometheus/exporter-toolkit v0.10.0
+	github.com/prometheus/procfs v0.12.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 )
